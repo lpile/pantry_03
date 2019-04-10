70ef3ef8b59d5a7b250941ed1536633c8b990418
lpile <logan.pile@gmail.com>
2019-04-10 10:09:25 -0600

    complete ingredient class and test

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -4,14 +4,16 @@ require './lib/ingredient'
 
 class IngredientTest <Minitest::Test
   def setup
-
+    @ingredient = Ingredient.new("Sugar", "3 oz", 55)
   end
 
   def test_it_exists
-
+    assert_instance_of Ingredient, @ingredient
   end
 
   def test_it_has_attributes
-
+    assert_equal "Sugar", @ingredient.name
+    assert_equal "3 oz", @ingredient.unit
+    assert_equal 55, @ingredient.calories
   end
 end
