6c84c660c41742e5c7d5691a6429272029e2d38a
lpile <logan.pile@gmail.com>
2019-04-10 10:44:00 -0600

    create skeleton pantry class and test

---
+++ test/pantry_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+require './lib/pantry'
+
+class PantryTest <Minitest::Test
+  def setup
+
+  end
+
+  def test_it_exists
+  end
+
+  def test_it_has_attributes
+  end
+end
