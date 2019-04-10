b9dcfb4445dbda7e5ae3d12f3babb57fa7d557ec
lpile <logan.pile@gmail.com>
2019-04-10 10:39:33 -0600

    update ingredient test with correct unit argument

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -4,7 +4,7 @@ require './lib/ingredient'
 
 class IngredientTest <Minitest::Test
   def setup
-    @ingredient = Ingredient.new("Sugar", "3 oz", 55)
+    @ingredient = Ingredient.new("Sugar", "oz", 55)
   end
 
   def test_it_exists
@@ -13,7 +13,7 @@ class IngredientTest <Minitest::Test
 
   def test_it_has_attributes
     assert_equal "Sugar", @ingredient.name
-    assert_equal "3 oz", @ingredient.unit
+    assert_equal "oz", @ingredient.unit
     assert_equal 55, @ingredient.calories
   end
 end
