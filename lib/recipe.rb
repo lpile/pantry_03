af6efc1c408aca79d860e0a50431f73b12dab165
lpile <logan.pile@gmail.com>
2019-04-10 10:38:58 -0600

    complete recipe class and test

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,3 +1,21 @@
 class Recipe
+  attr_reader :name, :ingredients
 
+  def initialize(name)
+    @name = name
+    @ingredients = {}
+  end
+
+  def add_ingredient(ingredient, quantity)
+    @ingredients.store(ingredient, quantity)
+  end
+
+  def total_calories
+    total_calories = {}
+    @ingredients.each do |ingredient|
+      total_calories[ingredient.first.name] = ingredient.first.calories * ingredient.last
+      # require 'pry'; binding.pry
+    end
+    total_calories.values.sum
+  end
 end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -6,6 +6,8 @@ require './lib/recipe'
 class RecipeTest <Minitest::Test
   def setup
     @recipe = Recipe.new("Cookies")
+    @ingredient_1 = Ingredient.new("Sugar", "oz", 55)
+    @ingredient_2 = Ingredient.new("Egg", "whole", 77)
   end
 
   def test_it_exists
@@ -15,5 +17,37 @@ class RecipeTest <Minitest::Test
   def test_it_has_attributes
     assert_equal "Cookies", @recipe.name
     assert_equal ({}), @recipe.ingredients
+    assert_equal 0, @recipe.total_calories
+  end
+
+  def test_it_adds_ingredients
+    @recipe.add_ingredient(@ingredient_1, 4)
+    expected = {@ingredient_1 => 4}
+
+    assert_equal expected, @recipe.ingredients
+  end
+
+  def test_it_adds_multiple_ingredients
+    @recipe.add_ingredient(@ingredient_1, 4)
+    @recipe.add_ingredient(@ingredient_2, 2)
+    expected = {@ingredient_1 => 4,
+                @ingredient_2 => 2}
+
+    assert_equal expected, @recipe.ingredients
+  end
+
+  def test_it_can_give_quantity_of_ingredient
+    @recipe.add_ingredient(@ingredient_1, 4)
+    @recipe.add_ingredient(@ingredient_2, 2)
+
+    assert_equal 4, @recipe.ingredients[@ingredient_1]
+    assert_equal 2, @recipe.ingredients[@ingredient_2]
+  end
+
+  def test_it_can_give_total_calories
+    @recipe.add_ingredient(@ingredient_1, 4)
+    @recipe.add_ingredient(@ingredient_2, 2)
+
+    assert_equal 374, @recipe.total_calories
   end
 end
