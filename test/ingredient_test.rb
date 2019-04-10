f824b82a88c4951e60d9cd2e9817b6813b8a572d
lpile <logan.pile@gmail.com>
2019-04-10 10:05:47 -0600

    create skeleton ingredient class and test

---
+++ test/ingredient_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+
+class IngredientTest <Minitest::Test
+  def setup
+
+  end
+
+  def test_it_exists
+
+  end
+
+  def test_it_has_attributes
+
+  end
+end
